977cb54c17a64156f94c78385c64875dc32a3104
ZuuZaa <zumbaghirova@gmail.com>
2022-09-19 14:44:02 +0400

    portfolio container

--- src/data/portfolio.jsx
+++ src/data/portfolio.jsx
@@ -1,11 +1,48 @@
-import TodoCover from "assets/images/todo.png"
+import TodoCover from "assets/images/todo.png";
+import PortfolioCover from "assets/images/portfolio.png";
+import LiftCover from "assets/images/lift.png";
+import SeljanCover from "assets/images/seljan.png";
+import XezanTvCover from "assets/images/xezanTv.png";
+
 
 export  const portfolioData = [
     {
         cover: TodoCover,
         title: "Todo List",
+        key: "todo",
         url: "https://mytodolistwithreact.netlify.app/",
-        "repo": "https://github.com/ZuuZaa/todo_react",
-        "description": "Raect Js, Context API, Theme toogling, Local Storage"
-    }
+        repo: "https://github.com/ZuuZaa/todo_react",
+        description: ["Raect Js", "Context API", "Theme toogling", "Local Storage"]
+    },
+    {
+        cover: PortfolioCover,
+        title: "Portfolio Page",
+        key: "portfolio",
+        url: "https://zumrudsportfolio.netlify.app/",
+        repo: "https://github.com/ZuuZaa/react-portfolio",
+        description: ["Raect Js", "Emailjs", "Leaflet Map"]
+    },
+    {
+        cover: XezanTvCover,
+        title: "HR management",
+        key: "hr",
+        url: "https://hr-management-erp.netlify.app",
+        repo: "https://github.com/ZuuZaa/xezan_tv",
+        description: ["Raect Js","Material UI", "Redux Saga", "Formik", "Date-fns"]
+    },
+    {
+        cover: LiftCover,
+        title: "Lift Info",
+        key: "lift",
+        url: "https://liftback.herokuapp.com/",
+        repo: "https://github.com/ZuuZaa/lift_info_python_django",
+        description: ["Python", "Django"]
+    },
+    {
+        cover: SeljanCover,
+        title: "Todo List",
+        key: "seljan",
+        url: "https://www.seljannasibli.com/",
+        repo: "https://github.com/ZuuZaa/seljan_nasibli",
+        description: ["Bootstrap", "Jquery", "Carousel"]    }
 ]
--- src/pages/PortfolioPage/index.jsx
+++ src/pages/PortfolioPage/index.jsx
@@ -19,7 +19,6 @@ const Portfolio = () => {
 
   return (
     <div className="portfolio-page">
-      <span className="tags">&lt;h1&gt;</span>
       <h1>
         <AnimatedLetters
           strArray={strAttay}
@@ -27,15 +26,23 @@ const Portfolio = () => {
           index={15}
         />
       </h1>
-      <span className="tags">&lt;/h1&gt;</span>
       <div className="portfolio-container">
-        {/* {portfolioData.map(item => (
-          <div className="portfolio-wrapper" key={item.key}>
-            <img src={item.cover} alt="" />
-            {item.key}
-      
+        {portfolioData.map(item => (
+          <div className="portfolio-card" key={item.key}>
+            <img src={item.cover} alt="cover" className="portfolio-cover" />
+            <div className="content">
+              <div className="content-header">
+                <div className="card-title">{item.title}</div>
+                <ul className="card-description">{item.description.map(item => <li>{item}</li>)}</ul>
+              </div>
+              <div className="buttons">
+                <a href={item.url} target="blank">demo</a>
+                <a href={item.repo} target="blank">repo</a>
+              </div>
+
+            </div>
           </div>
-        ))} */}
+        ))}
       </div>
     </div>
   )
